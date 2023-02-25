cf86cd31cf627ff4f974cfffde5fe205a915bac4
liborhavranek <liborhavranek91@gmail.com>
2023-02-25 16:11:57 +0100

    Song
    - add create checkfile
    
    [OOP]

--- song.py
+++ song.py
@@ -6,7 +6,7 @@ class Song:
 	duration: int, The duration of song in seconds
 	"""
 
-	def __init__(self, title, artist, duration):
+	def __init__(self, title, artist, duration=0):
 		"""
 		:param title: The title of the song
 		:param artist: Artist, Object representing a creator
@@ -21,22 +21,21 @@ class Song:
 """Show me documentation for everything in class"""
 print("Documentation for everything in class")
 help(Song)
-print(80*"-")
+print(80 * "-")
 
 """Show documentation for Init method in the class"""
 print("documentation for init method in class")
 help(Song.__init__)
-print(80*"-")
+print(80 * "-")
 
 """Show documentation for class"""
 print("show documentation for class")
 print(Song.__doc__)
-print(80*"-")
+print(80 * "-")
 
 """Show documentation for init in class """
 print("show documentation for init in class")
 print(Song.__init__.__doc__)
-
 
 Song.__init__.__doc__ = """This is added init doc"""
 help(Song.__init__)
@@ -54,6 +53,7 @@ class Album:
 	Methods:
 		add_song: Used to add a new song to the album's track list.
 	"""
+
 	def __init__(self, name, year, artist=None):
 		self.name = name
 		self.year = year
@@ -64,7 +64,7 @@ class Album:
 
 		self.tracks = []
 
-	def add_song(self, song, position):
+	def add_song(self, song, position=None):
 		"""Add song to the tracklist
 		Arguments:
 		song (Song): A song to add.
@@ -89,6 +89,7 @@ class Artist:
 	Methods:
 	add_album: Use to add a new album to the artist's albums list
 	"""
+
 	def __init__(self, name):
 		self.name = name
 		self.albums = []
@@ -113,8 +114,43 @@ def load_data():
 			# data row should consist of (artist, album, year, song)
 			artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
 			year_field = int(year_field)
-			print(artist_field, album_field, year_field, song_field)
+			print("Artist: {}\t Album: {}\t Year: {}\t Song: {}\t".format(artist_field, album_field, year_field, song_field))
+
+			if new_artist is None:
+				new_artist = Artist(artist_field)
+			elif new_artist.name != artist_field:
+				new_artist.add_album(new_album)
+				artist_list.append(new_artist)
+				new_artist = Artist(artist_field)
+				new_album = None
+
+			if new_album is None:
+				new_album = Album(album_field, year_field, new_artist)
+			elif new_album.name != album_field:
+				new_artist.add_album(new_album)
+				new_album = Album(album_field, year_field, new_artist)
+
+			new_song = Song(song_field, new_artist, 999)
+			new_album.add_song(new_song)
+
+		if new_artist is not None:
+			if new_album is not None:
+				new_artist.add_album(new_album)
+			artist_list.append(new_artist)
+
+	return artist_list
+
+
+def create_checkfile(artist_list):
+	"""Create a check file from the object data for comparison with the original file"""
+	with open("checkfile.txt", 'w') as checkfile:
+		for new_artist in artist_list:
+			for new_album in new_artist.albums:
+				for new_song in new_album.tracks:
+					print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song), file=checkfile)
 
 
 if __name__ == "__main__":
-	load_data()
+	artists = load_data()
+	print("There are {} aritsts".format(len(artists)))
+	create_checkfile(artists)
